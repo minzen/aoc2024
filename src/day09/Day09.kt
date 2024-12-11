0212cc151bdbdc4d06b1b60d7e2234ae98049037
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2024-12-11 21:02:09 +0100

    day11/part1

---
+++ src/day09/Day09.kt
@@ -0,0 +1,18 @@
+package day09
+
+import readInput
+import println
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val input = readInput("day09/Day09")
+    part1(input).println()
+    part2(input).println()
+}
